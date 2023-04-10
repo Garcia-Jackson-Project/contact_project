c8b916b5732e9a64bda268cea756eb3e0ca02071
Paul Garcia <paul.ngarcia@yahoo.com>
2023-04-10 16:43:57 -0500

    beginning, utils created, cli beginning created

---
+++ rollerdex/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main (String[] args){
+        Main interact = new Main();
+
+        interact.CLIcreate();
+    }
+
+    public void  CLIcreate(){
+        System.out.println("Welcome");
+        System.out.println("1. View contacts.");
+        System.out.println("2. Add a new contact.");
+        System.out.println("3. Search a contact by name.");
+        System.out.println("4. Delete an existing contact.");
+        System.out.println("5. Exit.");
+        System.out.println("Enter an option (1, 2, 3, 4 or 5):");
+
+
+    }
+
+
+
+
+
+
+}
